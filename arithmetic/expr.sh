92f7dc73d5162bb23e4b7f9f481d7e841cbf1bbf
parrotshen <parrotshen@yahoo.com.tw>
2021-01-08 15:22:05 +0800

    add "expr" example

---
+++ arithmetic/expr.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "22 + 11 = $(expr 22 + 11)"
+echo "22 - 11 = $(expr 22 - 11)"
+echo "22 * 11 = $(expr 22 \* 11)"
+echo "22 / 11 = $(expr 22 / 11)"
+echo "22 % 11 = $(expr 22 % 11)"
+
